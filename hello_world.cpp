9e186c8227b803ec1abd95cf04915d59836d3358
operatordoto <foxes_lore@mail.ru>
2026-06-07 20:39:55 +0300

    add hello_world with bad code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 int main()
 {
-  string name;
-  cout << "Enter your name: ";
-  cin >> name;
-  cout << "Hello world from @" << name << endl;
+  cout << "Hello world" << endl;
   return 0;
 }
