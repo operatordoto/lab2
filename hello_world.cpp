247bef54dce94fbee50ac7bb546268ab9210d859
operatordoto <foxes_lore@mail.ru>
2026-06-07 20:18:08 +0300

    add hello_world with bad code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
-int
-main()
+int main()
 {
-  std::string name;
-  // Request user name from standard input stream.
-  std::cout << "Enter your name: ";
-  std::cin >> name;
-  // Print greeting to standard output stream.
-  std::cout << "Hello world from @" << name << std::endl;
+  cout << "Hello world" << endl;
   return 0;
 }
