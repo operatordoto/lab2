24c1cca87c054f2116b066086ac181c347e25f0f
operatordoto <foxes_lore@mail.ru>
2026-06-07 20:26:56 +0300

    add hello_world with bad code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
 int main()
 {
-  std::string name;
-  // Запрашиваем имя пользователя через стандартный поток ввода
-  std::cout << "Enter your name: ";
-  std::cin >> name;
-  // Выводим приветствие в стандартный поток вывода
-  std::cout << "Hello world from @" << name << std::endl;
+  cout << "Hello world" << endl;
   return 0;
 }
