a784c9dfb2e2078aa334b1cde27cc4089837b15f
operatordoto <foxes_lore@mail.ru>
2026-06-07 20:49:23 +0300

    fix: remove using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,10 +4,8 @@
 int main()
 {
   std::string name;
-  // Запрашиваем имя пользователя через стандартный поток ввода
   std::cout << "Enter your name: ";
   std::cin >> name;
-  // Выводим приветствие в стандартный поток вывода
   std::cout << "Hello world from @" << name << std::endl;
   return 0;
 }
